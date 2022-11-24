fa1b858453de73dd915a5ebc6c14de5499b99c87
Григорий <grinrb@mail.ru>
2022-11-24 20:30:01 +0500

    2. Модель данных. [#174683]

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,11 @@
+package ru.job4j.pojo;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setFullName("Kashin Grigorii Alesandrovich");
+        student.setNumberGroup("PS-112");
+        student.setReceipt("01 августа 2006 года");
+        System.out.println(student.getFullName() + "\n" + student.getNumberGroup() + "\n" + student.getReceipt());
+    }
+}
---
+++ src/main/java/ru/job4j/pojo/Student.java
@@ -0,0 +1,31 @@
+package ru.job4j.pojo;
+
+public class Student {
+    private String fullName;
+    private String numberGroup;
+    private String receipt;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getNumberGroup() {
+        return numberGroup;
+    }
+
+    public void setNumberGroup(String numberGroup) {
+        this.numberGroup = numberGroup;
+    }
+
+    public String getReceipt() {
+        return receipt;
+    }
+
+    public void setReceipt(String receipt) {
+        this.receipt = receipt;
+    }
+}

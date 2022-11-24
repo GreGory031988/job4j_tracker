986b13cc7a6285ab3810b6d15de5efd0437204ea
Григорий <grinrb@mail.ru>
2022-11-24 23:51:28 +0500

    3. Массивы и модели. [#174892]

---
+++ src/main/java/ru/job4j/pojo/Book.java
@@ -0,0 +1,27 @@
+package ru.job4j.pojo;
+
+public class Book {
+    private String name;
+    private int page;
+
+    public Book(String name, int page) {
+        this.name = name;
+        this.page = page;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+}
---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,34 @@
+package ru.job4j.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book moron = new Book("Moron", 369);
+        Book demons = new Book("Demons", 234);
+        Book brothers = new Book("brothers", 793);
+        Book cleanCode = new Book("CleanCode", 452);
+        Book[] titles = new Book[4];
+        titles[0] = moron;
+        titles[1] = demons;
+        titles[2] = brothers;
+        titles[3] = cleanCode;
+        for (int index = 0; index < titles.length; index++) {
+            Book title = titles[index];
+            System.out.println(title.getName() + " - " + title.getPage());
+        }
+        System.out.println("Change place 0 and 3");
+        Book temp = titles[0];
+        titles[0] = titles[titles.length - 1];
+        titles[titles.length - 1] = temp;
+        for (int index = 0; index < titles.length; index++) {
+            Book title = titles[index];
+            System.out.println(title.getName() + " - " + title.getPage());
+        }
+        System.out.println("Show book only name Clean Code");
+        for (int index = 0; index < titles.length; index++) {
+            Book title = titles[index];
+            if (title.getName().equals("CleanCode")) {
+                System.out.println(title.getName() + " - " + title.getPage());
+            }
+        }
+    }
+}

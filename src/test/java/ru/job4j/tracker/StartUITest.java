85ad25cf9012bde8652bfa4e09fe7dd20619fad0
Григорий <grinrb@mail.ru>
2022-12-14 02:21:37 +0500

    11.1. Реализация меню за счет шаблона стратегия. [#181780]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,48 +1,5 @@
 package ru.job4j.tracker;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
 public class StartUITest {
 
-    @Test
-    public void whenAddItem() {
-            String[] answers = {"Fix PC"};
-            Input input = new StubInput(answers);
-            Tracker tracker = new Tracker();
-            StartUI.createItem(input, tracker);
-            Item created = tracker.findAll()[0];
-            Item expected = new Item("Fix PC");
-            assertThat(created.getName()).isEqualTo(expected.getName());
-        }
-
-    @Test
-    public void whenEditItem() {
-        Tracker tracker = new Tracker();
-        Item item = new Item("new item");
-        tracker.add(item);
-        String[] answers = {
-                String.valueOf(item.getId()),
-                "edited item"
-        };
-        StartUI.editItem(new StubInput(answers), tracker);
-        Item edited = tracker.findById(item.getId());
-        assertThat(edited.getName()).isEqualTo("edited item");
-    }
-
-    @Test
-    public void whenDeleteItem() {
-        Tracker tracker = new Tracker();
-        Item item = new Item("new item");
-        tracker.add(item);
-        String[] answers = {
-                String.valueOf(item.getId()),
-                "deleted item"
-        };
-        StartUI.deleteItem(new StubInput(answers), tracker);
-        Item deleted = tracker.findById(item.getId());
-        Assertions.assertNull(deleted);
-    }
 }

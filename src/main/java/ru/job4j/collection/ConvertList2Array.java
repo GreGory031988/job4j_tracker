29b403c13ccb7ee7e12070a982a2bc43a045208b
Григорий <grinrb@mail.ru>
2023-01-20 00:08:28 +0500

    6. Конвертация ArrayList в двухмерный массив [#10035 #383031]

--- src/main/java/ru/job4j/collection/ConvertList2Array.java
+++ src/main/java/ru/job4j/collection/ConvertList2Array.java
@@ -16,7 +16,7 @@ public class ConvertList2Array {
             }
         }
             return array;
-        }
+    }
 
     public static void main(String[] args) {
         List<Integer> list = List.of(1, 2, 3, 4, 5, 6, 7);

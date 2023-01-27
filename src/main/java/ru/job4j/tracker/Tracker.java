18bcfb1074465b47ecf7943a7c025f2572422130
Григорий <grinrb@mail.ru>
2023-01-27 22:52:05 +0500

    9. Изменить программу Tracker из 2-го модуля [#10039 #384963]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -6,7 +6,6 @@ import java.util.List;
 public class Tracker {
     private final List<Item> items = new ArrayList<>();
     private int ids = 1;
-    private int size = 0;
 
     public Item add(Item item) {
         item.setId(ids++);

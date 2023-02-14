b0288a4cfd3d1240ef5322c9aa8b4fb4bc13afa0
Григорий <grinrb@mail.ru>
2023-02-14 23:15:21 +0500

    4. Аттестация [#504878 #390908]

--- src/main/java/ru/job4j/hmap/AnalyzeByMap.java
+++ src/main/java/ru/job4j/hmap/AnalyzeByMap.java
@@ -4,17 +4,15 @@ import java.util.*;
 
 public class AnalyzeByMap {
     public static double averageScore(List<Pupil> pupils) {
-        double averageScore = 0D;
         double score = 0D;
         int count = 0;
         for (Pupil pupil : pupils) {
             for (Subject subject : pupil.subjects()) {
                 count++;
                 score += subject.score();
-                averageScore = score / count;
             }
         }
-        return averageScore;
+        return score / count;
     }
 
     public static List<Label> averageScoreByPupil(List<Pupil> pupils) {
@@ -36,8 +34,7 @@ public class AnalyzeByMap {
         Map<String, Integer> map = new LinkedHashMap<>();
         for (Pupil pupil : pupils) {
             for (Subject subject : pupil.subjects()) {
-                int score = map.getOrDefault(subject.name(), 0);
-                score += subject.score();
+                int score = map.getOrDefault(subject.name(), 0) + subject.score();
                 map.put(subject.name(), score);
             }
         }
@@ -57,8 +54,8 @@ public class AnalyzeByMap {
             }
             list.add(new Label(pupil.name(), score));
             score = 0;
-            list.sort(Comparator.naturalOrder());
         }
+        list.sort(Comparator.naturalOrder());
         return list.get(list.size() - 1);
     }
 
@@ -74,8 +71,8 @@ public class AnalyzeByMap {
         }
         for (Map.Entry<String, Integer> mapEntry : map.entrySet()) {
             list.add(new Label(mapEntry.getKey(), (double) mapEntry.getValue()));
-            list.sort(Comparator.naturalOrder());
         }
+        list.sort(Comparator.naturalOrder());
         return list.get(list.size() - 1);
     }
 }

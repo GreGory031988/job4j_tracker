3a7b812993f1d0c30e4efaca42094d764f5fa31e
Григорий <grinrb@mail.ru>
2023-02-15 13:49:37 +0500

    0. Сортировка [#10096]

---
+++ src/main/java/ru/job4j/collection/ListSort.java
@@ -0,0 +1,16 @@
+package ru.job4j.collection;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class ListSort {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(2, 4, 3, 5, 1);
+        System.out.println(list);
+        Collections.sort(list);
+        System.out.println(list);
+        Collections.sort(list, Collections.reverseOrder());
+        System.out.println(list);
+    }
+}

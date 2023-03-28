ffb3da12aa8a38066dc455ef6137bc2c20032eff
GreGory031988 <grinrb@mail.ru>
2023-03-28 06:33:43 +0500

    5. Фильтрация учеников [#110058 #404737]

--- src/main/java/ru/job4j/stream/School.java
+++ src/main/java/ru/job4j/stream/School.java
@@ -6,6 +6,8 @@ import java.util.stream.Collectors;
 
 public class School {
     public List<Student> collect(List<Student> students, Predicate<Student> predicate) {
-        return students.stream().filter(predicate).collect(Collectors.toList());
+        return students.stream()
+                .filter(predicate)
+                .collect(Collectors.toList());
     }
 }

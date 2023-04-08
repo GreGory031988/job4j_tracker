f5ad833be2d08a74aab0441ce23f30151b3ffb5d
Григорий <grinrb@mail.ru>
2023-04-08 10:08:46 +0500

    2. Local-Variable Type Inference [#70626]

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -17,7 +17,7 @@ public class PhoneDictionary {
         Predicate<Person> getAddress = (i) -> (i).getAddress().contains(key);
         Predicate<Person> combine = getName.or(getSurname).or(getPhone).or(getAddress);
         ArrayList<Person> result = new ArrayList<>();
-        for (Person person : persons) {
+        for (var person : persons) {
             if (combine.test(person)) {
                 result.add(person);
             }
--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -2,18 +2,16 @@ package ru.job4j.search;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class PhoneDictionaryTest {
     @Test
     public void whenFindByName() {
-        PhoneDictionary phones = new PhoneDictionary();
+        var phones = new PhoneDictionary();
         phones.add(
                 new Person("Petr", "Arsentev", "534872", "Bryansk")
         );
-        ArrayList<Person> persons = phones.find("Petr");
+        var persons = phones.find("Petr");
         assertThat(persons.get(0).getSurname()).isEqualTo("Arsentev");
     }
 }
